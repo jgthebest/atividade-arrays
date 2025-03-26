bf4b9314e0975510d4b09c085c255d9d3656f9e3
jgthebest <jgm.fernandes222@gmail.com>
2025-03-26 10:28:49 -0300

    Atividade Arrays

--- src/Main.java
+++ src/Main.java
@@ -16,25 +16,25 @@ public class Main {
 
 //public class Main {
 //    public static void main(String[] args){
-////        double[] notas = {7.0, 8.5, 9.0, 6.0, 7.5};
-////        double soma = 0;
-////        for (double nota : notas) {
-////            soma += nota;
-////        }
-////        System.out.printf("Média da turma: %.2f", (soma / notas.length));
+//        double[] notas = {7.0, 8.5, 9.0, 6.0, 7.5};
+//        double soma = 0;
+//        for (double nota : notas) {
+//            soma += nota;
+//        }
+//        System.out.printf("Média da turma: %.2f", (soma / notas.length));
 //
 //        int[][] matriz = {
 //                {1,2,3},
 //                {4,5,6},
 //                {7,8,9}
 //        };
-////        metodo convencional:
-////        for (int i = 0; i < matriz.length; i++){
-////            for (int j = 0; j < matriz[i].length; j++){
-////                System.out.println(matriz[i][j]);
-////            }
-////        }
-////        metodo limpo:
+//        metodo convencional:
+//        for (int i = 0; i < matriz.length; i++){
+//            for (int j = 0; j < matriz[i].length; j++){
+//                System.out.println(matriz[i][j]);
+//            }
+//        }
+//        metodo limpo:
 //        for (int[] linha : matriz){
 //            for (int elemento : linha){
 //                System.out.println(elemento);
